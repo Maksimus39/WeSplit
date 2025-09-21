40503c5de16d11245869784ea788d3fb02abc498
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-21 14:36:49 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Максим Минаков on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
