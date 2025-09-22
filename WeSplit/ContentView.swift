476e1f10eca697e1677cf6d8acccb001a8f4b17f
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-09-21 21:24:50 -0700

    homework 1

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -16,38 +16,27 @@ struct ContentView: View {
     @State private var selectedStudent = "Harry"
     
     var body: some View {
-        
-        
         NavigationStack {
             Form {
                 Section {
                     TextField("Enter your name", text: $name)
                     Text("Hello, world!: \(name)")
+                    Picker("Select your student", selection: $selectedStudent){
+                        ForEach(students, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    Button("Tap Count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
                 }
-                .navigationTitle("SwiftUI")
+                .navigationTitle("Моё первое приложение")
                 .navigationBarTitleDisplayMode(.large)
             }
         }
-        Button("Tap Count: \(tapCount)") {
-            self.tapCount += 1
-        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         
-        Form {
-            ForEach(0 ..< 100) {
-                Text("Row \($0)")
-            }
-        }
-        
-        NavigationStack {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-            }
-        }
-        
+
     }
 }
 
